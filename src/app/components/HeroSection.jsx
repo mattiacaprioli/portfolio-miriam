cd03060b8749eec6c51c7d95db0e142f20c26677
Mattia Caprioli <capriolimattia1994@gmail.com>
2024-09-28 19:40:12 +0200

    fix minor bugs

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -2,19 +2,13 @@
 import React from "react";
 import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
-import { motion } from "framer-motion";
 import Link from "next/link";
 
 const HeroSection = () => {
   return (
     <section className="lg:py-16">
       <div className="grid grid-cols-1 sm:grid-cols-12">
-        <motion.div
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
-          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
-        >
+        <div className="col-span-8 place-self-center text-center sm:text-left justify-self-start" >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
               Hello, I&apos;m{" "}
@@ -56,13 +50,8 @@ const HeroSection = () => {
               </span>
             </Link>
           </div>
-        </motion.div>
-        <motion.div
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
-          className="col-span-4 place-self-center mt-4 lg:mt-0"
-        >
+        </div>
+        <div className="col-span-4 place-self-center mt-4 lg:mt-0" >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
               src="/images/hero-image.png"
@@ -72,7 +61,7 @@ const HeroSection = () => {
               height={300}
             />
           </div>
-        </motion.div>
+        </div>
       </div>
     </section>
   );
