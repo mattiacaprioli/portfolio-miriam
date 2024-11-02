e07a381a97a9aa87a7c807c48bdd55f195e1ddcb
Mattia Caprioli <capriolimattia1994@gmail.com>
2024-11-02 17:21:48 +0100

    feat user experience in navBar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -7,41 +7,36 @@ import { IoMdClose } from "react-icons/io";
 import MenuOverlay from './MenuOverlay';
 
 const navLinks = [
-  {
-    title: 'Chi sono',
-    path: '#about'
-  },
-  {
-    title: 'Progetti',
-    path: '#projects'
-  },
-  {
-    title: 'Contatti',
-    path: '#contact'
-  }
-]
+  { title: 'Chi sono', path: '#about' },
+  { title: 'Esperienza', path: '#experience' },
+  { title: 'Progetti', path: '#projects' },
+  { title: 'Contatti', path: '#contact' }
+];
 
 function Navbar() {
   const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const handleLinkClick = () => {
+    setNavbarOpen(false);
+  };
 
   return (
     <nav className='fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-50 bg-[#121212] bg-opacity-100'>
       <div className='flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2'>
         <Link href="/" className='text-2xl md:text-5xl text-white font-semibold'>
-          
+          {/* Logo */}
         </Link>
         <div className='mobile-menu block md:hidden'>
-          {
-            navbarOpen ? (
-              <button onClick={() => setNavbarOpen(false)} className='flex items-center px-3 py-2 rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'>
-                <IoMdClose className='h-5 w-5'/>
-              </button>
+          <button 
+            onClick={() => setNavbarOpen(!navbarOpen)} 
+            className='flex items-center px-3 py-2 rounded border-slate-200 text-slate-200 hover:text-white hover:border-white transition-transform duration-300'
+          >
+            {navbarOpen ? (
+              <IoMdClose className='h-5 w-5 animate-icon-close z-10' />
             ) : (
-              <button onClick={() => setNavbarOpen(true)} className='flex items-center px-3 py-2 rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'>
-                <RxHamburgerMenu className='h-5 w-5'/>
-              </button>
-            )
-          }
+              <RxHamburgerMenu className='h-5 w-5 animate-icon-open' />
+            )}
+          </button>
         </div>
         <div className="menu hidden md:block md:w-auto" id='navbar'>
           <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0'>
@@ -53,9 +48,10 @@ function Navbar() {
           </ul>
         </div>
       </div>
-      {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
+      <MenuOverlay links={navLinks} isVisible={navbarOpen} onLinkClick={handleLinkClick} />
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
+
